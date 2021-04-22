2e8f4d5b73cc3cfbbec11255489729bcd4358131
Calum Slee <calumslee@hotmail.co.nz>
2021-04-23 10:57:22 +1200

    Player prototype added

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Take the current position = New Position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
