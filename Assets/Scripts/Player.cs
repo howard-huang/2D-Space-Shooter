bef65b0d8773fc23dad4318096361b20eaa767d3
Calum Slee <calumslee@hotmail.co.nz>
2021-04-23 17:25:21 +1200

    Player bounds added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,18 +10,34 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Take the current position = New Position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-        
+       
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0).normalized;             //normalized to stop axes giving a range of values
+
         transform.Translate(direction * _speed * Time.deltaTime);
 
+        float _yClamp = Mathf.Clamp(transform.position.y, -5.25f, 0);                              //vertical bounds
+        transform.position = new Vector3(transform.position.x, _yClamp, 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }                                                      //horizontal wrapping
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
