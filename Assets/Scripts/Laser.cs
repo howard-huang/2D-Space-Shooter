9da761b793a059a802f7d00d9e025b95251d0684
Calum Slee <calumslee@hotmail.co.nz>
2021-04-26 10:28:06 +1200

    Laser added

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    //speed variable of 8
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    void Update()
+    {
+        Movement();
+        Destroy();
+    }
+
+    void Movement()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+
+    void Destroy()
+    {
+        if (transform.position.y > 8)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,16 +7,27 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed = 3.5f;
 
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _laserPrefab;
+    private Vector3 _laserOffset = new Vector3(0f, 0.8f, 0f);
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1.0f;
+    
+
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0); 
+        transform.position = new Vector3(0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
         Movement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
     }
 
     void Movement()
@@ -25,19 +36,25 @@ public class Player : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
        
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0).normalized;             //normalized to stop axes giving a range of values
-
+                                                                                                   //more arcade like & stops faster diag movement
         transform.Translate(direction * _speed * Time.deltaTime);
 
         float _yClamp = Mathf.Clamp(transform.position.y, -5.25f, 0);                              //vertical bounds
         transform.position = new Vector3(transform.position.x, _yClamp, 0);
-
+                                                                                                   //horizontal wrapping
         if (transform.position.x >= 11.3f)
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
-        }                                                      //horizontal wrapping
+        }                                                      
         else if (transform.position.x <= -11.3f)
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    void FireLaser()                                                                               //laser instantiate + cooldown
+    {
+        _canFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + _laserOffset, Quaternion.identity);
     }
 }
