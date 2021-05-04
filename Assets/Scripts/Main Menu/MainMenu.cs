78d85b1ec156fc294a84415cfd951af05fb164c3
Calum Slee <calumslee@hotmail.co.nz>
2021-05-04 15:24:48 +1200

    Main Menu Added

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1); //Game Scene
+    }
+}
