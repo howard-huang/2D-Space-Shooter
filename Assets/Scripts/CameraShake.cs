b469d0b0ee986f502caa46f5c1bc10c92dc944c9
Calum Slee <calumslee@hotmail.co.nz>
2021-06-07 14:50:39 +1200

    Camera Shake on Damage added

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public void Damage()
+    {
+        StartCoroutine(StartShake());
+    }
+
+    private IEnumerator StartShake()
+    {
+        Vector3 _defaultCameraPos = this.transform.position;
+        float _shakeTime = Time.time;
+        float _shakeLength = _shakeTime + 0.3f;
+
+        while (_shakeTime < _shakeLength)
+        {
+            _shakeTime += Time.deltaTime;
+            float _xShake = RandomNumber(-0.5f, 0.5f);
+            float _yShake = RandomNumber(-0.5f, 0.5f);
+            Vector3 _shakePos = new Vector3(_xShake, _yShake, transform.position.z);
+            transform.position = _shakePos;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    private float RandomNumber(float _min, float _max)
+    {
+        float _randomNumber = Random.Range(_min, _max);
+        return _randomNumber;
+    }
+}
